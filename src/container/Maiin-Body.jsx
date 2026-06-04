1296084aec1aeb6316820f74908e5c46e0ca5535
Anshumita Sahu <anshumitasahu.official@gmail.com>
2026-06-04 18:20:00 +0530

    added buttons to costomize the text in the text area

--- src/container/Maiin-Body.jsx
+++ src/container/Maiin-Body.jsx
@@ -5,16 +5,40 @@ export default function MainBody({ myStyle }) {
     const wordCount =
         text.trim() === "" ? 0 : text.trim().split(/\s+/).length
 
+    const clearText = () => {
+        setText("");
+    }
+
+    const handleUpperCase = () => {
+        setText(text.toUpperCase());
+    }
+
+    const handleLowerCase = () => {
+        setText(text.toLowerCase());
+    }
+
+    const handleCapital = () => {
+        const capitalised = text.toLowerCase().split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+        setText(capitalised);
+    }
+
+
     return (
         <>
             <div className="preview">
                 <div className="input-elem">
                     <h1>Enter Your Text To Know About It </h1>
-                    <textarea type="text" className="input-box" value={text} onChange={(e) => setText(e.target.value)} 
+                    <textarea type="text" className="input-box" value={text} onChange={(e) => setText(e.target.value)}
                         style={{
                             backgroundColor: myStyle.backgroundColor === "black" ? "black" : " white",
-                            color: myStyle.color === "white" ? "white" : "blavk"
+                            color: myStyle.color === "white" ? "white" : "black"
                         }} ></textarea>
+                </div>
+                <div className="button-area">
+                    <button onClick={clearText} className="btn clear-btn">Clear Text</button>
+                    <button onClick={handleUpperCase} className="btn uppCase">Convert to Uppercase</button>
+                    <button onClick={handleLowerCase} className="btn lowCase">Convert to Lowercase</button>
+                    <button onClick={handleCapital} className="btn capital">Capitalze</button>
                 </div>
                 <div className="text-length">
                     <h4>Your  Text Summary</h4>
